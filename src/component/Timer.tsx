b406cd09d3af6db469766221158cb6bb7f223369
margaritasing <libiamfreitesm@gmail.com>
2022-11-25 19:40:47 -0300

    agrego funcionalidades del useRef

--- src/component/Timer.tsx
+++ src/component/Timer.tsx
@@ -1,9 +1,24 @@
+import { useEffect, useRef, useState } from "react";
+
+type TimerArgs = {
+    milisegundos: number;
+}
 
 
-export const Timer = () => {
+export const Timer = ( { milisegundos }: TimerArgs) => {
+
+    const [segundos, setSegundos] = useState(0);
+    const ref = useRef<NodeJS.Timeout>()
+
+    useEffect(() => {
+       ref.current && clearInterval( ref.current ) 
+       ref.current = setInterval(()=> setSegundos( s => s + 1), milisegundos);     
+    }, [milisegundos])
+    
+    
   return (
     <>
-        <h4>Timer: <small>0</small></h4>
+        <h4>Timer: <small>{ segundos }</small></h4>
     </>
   )
 }
--- src/component/TimerPadre.tsx
+++ src/component/TimerPadre.tsx
@@ -1,11 +1,26 @@
+import { useState } from "react"
 import { Timer } from "./Timer"
 
 
 export const TimerPadre = () => {
+const [milisegundos, setMilisegundos] = useState(0)
+
+
   return (
     <>
-        <span>Milisegundos {1000}</span>
-        <Timer/>        
+        <span>Milisegundos {milisegundos}</span>
+        <br/>
+        <button
+        onClick={ () => setMilisegundos(1000)}
+        className="btn btn-outline-success">
+            1000
+        </button>
+        <button
+        onClick={ () => setMilisegundos(2000)}
+        className="btn btn-outline-success">
+            2000
+        </button>
+        <Timer milisegundos={ milisegundos}  />        
     </>
   )
 }
